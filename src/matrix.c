9ab92777cf15c9019907ed0567d962065287f4e7
Sajjad Ahmed <sajjadahmed8269@gmail.com>
2026-01-11 11:04:47 +0500

    add create_matrix in matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "matrix.h"
+
+// Create a matrix of size n x n
+Matrix create_matrix(int n)
+{
+    Matrix m;
+    m.size = n;
+    m.data = (double **)malloc(n * sizeof(double *));
+    for (int i = 0; i < n; i++)
+    {
+        m.data[i] = (double *)malloc(n * sizeof(double));
+    }
+    return m;
+}
