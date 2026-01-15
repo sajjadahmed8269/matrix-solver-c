3dd82bd85986a861d68f1bbad032cf461370f8ef
Sajjad Ahmed <sajjadahmed8269@gmail.com>
2026-01-15 22:24:57 +0500

    Complete calculate_determinant function

--- src/matrix.c
+++ src/matrix.c
@@ -36,4 +36,34 @@ double calculate_determinant(Matrix *m)
     {
         return (m->data[0][0] * m->data[1][1]) - (m->data[0][1] * m->data[1][0]);
     }
+    else
+    {
+        double det = 0;
+        int sign = 1;
+        for (int col = 0; col < m->size; col++)
+        {
+            // create matrix of size n-1
+            Matrix sub = create_matrix(m->size - 1);
+            // fill the sub_matrix
+            for (int i = 1; i < m->size; i++)
+            {
+                int sub_col = 0;
+                for (int j = 0; j < m->size; j++)
+                {
+                    if (j == col)
+                    {
+                        continue; // skips the column
+                    }
+                    sub.data[i - 1][sub_col] = m->data[i][j];
+                    sub_col++;
+                }
+            }
+            // Add determinants together
+            det += sign * m->data[0][col] * calculate_determinant(&sub);
+            // change the sign
+            sign = -sign;
+            // free sub matrix
+            free_matrix(sub);
+        }
+    }
 }
