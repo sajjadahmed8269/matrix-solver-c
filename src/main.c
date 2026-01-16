07c832f18f52f3964efc639bbf4b4de97a58236b
Sajjad Ahmed <sajjadahmed8269@gmail.com>
2026-01-16 22:07:08 +0500

    Added ASCII art and greeting logic.

--- src/main.c
+++ src/main.c
@@ -3,6 +3,16 @@
 
 int main(void)
 {
+    // Greetings & Description
+    printf("\033[0;32m");
+    printf(" __  __       _        _      ____       _                \n");
+    printf("|  \\/  | __ _| |_ _ __(_)_  _/ ___|  ___| |_   _____ _ __ \n");
+    printf("| |\\/| |/ _` | __| '__| \\ \\/ / \\___ \\ / _ \\ \\ \\ / / _ \\ '__|\n");
+    printf("| |  | | (_| | |_| |  | |>  <  ___) |  __/ |\\ V /  __/ |   \n");
+    printf("|_|  |_|\\__,_|\\__|_|  |_/_/\\_\\|____/ \\___|_| \\_/ \\___|_|   \n");
+    printf("\033[0m");
+    printf("\nThis tool solves NxN systems using Gauss elimination method.");
+    printf("\n------------------------------------------------------------\n\n");
 
     return 0;
 }
